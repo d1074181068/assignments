77ff790fea1ccdae1cbdfbbc197a89de13a5d745
d1074181068 <wiwi19991214@gmail.com>
2022-07-28 13:24:57 +0800

    adjust log

--- Part2/assignment5.js
+++ Part2/assignment5.js
@@ -6,9 +6,9 @@ function delayedResult(n1,n2,delayTime,callback){
 }
 
 delayedResult(4,5,3000,function(result){
-    console.log(result)
+    console.log('asg5 :',result)
 })
 
 delayedResult(-5,10,2000,function(result){
-    window.alert(result)
+    window.alert(`asg5 : ${result}`)
 })
